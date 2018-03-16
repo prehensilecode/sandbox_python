48474920f740abf9f840c6c37671d47247d07215
David Chin <david.w.h.chin@gmail.com>
2018-03-16 12:41:17 -0400

    Fix quote problem

--- sqlite3/foosqlite.py
+++ sqlite3/foosqlite.py
@@ -12,7 +12,7 @@ c.execute('''CREATE TABLE stocks
              (date text, trans text, symbol text, qty real, price real)''')
 
 # insert row
-c.execute('INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100, 35.14)')
+c.execute("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100, 35.14)")
 
 # commit changes
 conn.commit()
