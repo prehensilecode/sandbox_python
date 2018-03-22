470af331151b9154f817f9e0430aa205cc283feb
David Chin <david.w.h.chin@gmail.com>
2018-03-22 13:41:48 -0400

    Example many-to-many with python sqlite3

--- sqlite3/many_to_many/foobar.py
+++ sqlite3/many_to_many/foobar.py
@@ -13,7 +13,7 @@ with sqlite3.connect('foobar.db') as conn:
         );''')
     c.execute('''CREATE TABLE bar(
         id INTEGER PRIMARY KEY NOT NULL,
-        bar_col TEXT NOT NULL
+        bar_col INTEGER NOT NULL
         );''')
     c.execute('''CREATE TABLE foobar(
             foo_id INTEGER,
@@ -25,4 +25,44 @@ with sqlite3.connect('foobar.db') as conn:
     c.execute('''CREATE INDEX barindex ON foobar(bar_id);''')
     conn.commit()
 
+    foos = []
+    for i in range(6):
+        foos.append((None, i+100))
+    
+    c.executemany('INSERT INTO foo VALUES (?,?)', foos)
+    conn.commit()
+
+    print('foos')
+    for row in c.execute('SELECT * FROM foo ORDER BY id'):
+        print(row)
+    print("")
+
+    bars = []
+    for i in range(6):
+        bars.append((None, i+200))
+
+    c.executemany('INSERT INTO bar VALUES (?,?)', bars)
+    conn.commit()
+
+    print('bars')
+    for row in c.execute('SELECT * FROM bar ORDER BY id'):
+        print(row)
+    print("")
+
+    foobars = []
+    for i in range(6):
+        #print('foobars: i = {}'.format(i))
+        foobars.append((i, 5-i))
+
+    c.executemany('INSERT INTO foobar VALUES (?,?)', foobars)
+    conn.commit()
+
+    print('foobars')
+    for row in c.execute('SELECT * FROM foobar ORDER BY foo_id'):
+        print('type(row) = {}, row = {}'.format(type(row), row))
+
+    print("")
+
+conn.close()
+os.remove('foobar.db')
 
