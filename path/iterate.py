e9256fb45a775ea6e3efc43e6a87613f0b0d1c5a
David Chin <david.w.h.chin@gmail.com>
2018-04-16 18:09:51 -0400

    Trying out pathlib

---
+++ path/iterate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.6
+import sys
+import os
+import pathlib
+
+p = pathlib.Path('..')
+
+for x in list(p.glob('*.py')):
+    print(x)
+
+print('- - - - - - - - - - - - - - - - - - -')
+
+for y in list(p.glob('**/*.py')):
+    print(y)
+
