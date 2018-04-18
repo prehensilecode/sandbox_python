3f21f9cd61d3f7df5ac89b175a9a13b87dbcb1f9
David Chin <david.w.h.chin@gmail.com>
2018-04-18 15:07:24 -0400

    Test smtplib and email

---
+++ email/testemail.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys, os
+import smtplib
+from email.message import EmailMessage
+
+with open('mail_contents.txt', 'r') as fp:
+    msg = EmailMessage()
+    msg.set_content(fp.read())
+
+msg['Subject'] = 'The contents of mail_contents.txt'
+msg['From'] = 'dwc62@drexel.edu'
+msg['To'] = 'david.chin@drexel.edu'
+
+s = smtplib.SMTP('localhost')
+s.send_message(msg)
+s.quit()
+
+
