f7a01b394d9202db8f0d620a857b32ccf99d06dd
David Chin <david.w.h.chin@gmail.com>
2018-05-25 12:39:01 -0400

    one plot per PI

--- matplotlib/bar_colors.py
+++ matplotlib/bar_colors.py
@@ -3,7 +3,9 @@ import sys, os
 import csv
 from itertools import cycle, islice
 import pandas as pd
-import matplotlib
+import matplotlib as mpl
+mpl.use('Agg')
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.ticker as mtick
@@ -12,7 +14,7 @@ import numpy as np
 from numpy.random import rand
 import argparse
 
-matplotlib.style.use('ggplot')
+mpl.style.use('ggplot')
 
 def testing():
     # https://stackoverflow.com/questions/18903750/vary-the-color-of-each-bar-in-bargraph-using-particular-value
@@ -51,13 +53,28 @@ def testing2():
     fig.savefig('testing2.svg')
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Make barchart from csv.')
-    parser.add_argument('-d', '--debug', help='Debugging output', action='store_true')
-    parser.add_argument('reportcsv', type=argparse.FileType('r'), help='Summary usage csv file')
-    args = parser.parse_args()
+def testing3(charges_df):
+    fig, ax = plt.subplots()
 
-    charges_df = pd.read_csv(args.reportcsv)
+    print('charges_df')
+    print(charges_df)
+    print('')
+    print('charges_df.index')
+    print(charges_df.index)
+    print('')
+    print('charges_df.values')
+    print(charges_df.values)
+    print('')
+
+    ax.bar(charges_df.index, charges_df.values, width=0.72)
+    plt.title('USING PLT DIRECTLY')
+    plt.tight_layout()
+    fig.savefig('foo_plt.svg')
+
+
+def make_all_plots(reportcsv):
+    print(type(reportcsv))
+    charges_df = pd.read_csv(reportcsv)
 
     print(charges_df.describe())
     print('')
@@ -68,49 +85,76 @@ def main():
     cpu_charges_df = charges_df[mask][['Last name', 'CPU charge ($)']].sort_values(by=['CPU charge ($)'], ascending=False)
 
     mycolors = []
+    pis = []
     for i,r in cpu_charges_df.iterrows():
-        print('i= {} ;  r[0] = {}'.format(i, r[0]))
-        if r[0] == 'Sohlberg':
-            #mycolors.append('red')
-            mycolors.append([1,0.2,0.2,1])
-        else:
-            #mycolors.append('grey')
-            mycolors.append([0.8,0.8,0.9,1])
+        pis.append(r[0])
 
-    print('len(cpu_charges_df) = {}'.format(len(cpu_charges_df)))
-    print('len(mycolors) = {}'.format(len(mycolors)))
-    print('my colors = {}'.format(mycolors))
+    for pi in pis:
+        for i,r in cpu_charges_df.iterrows():
+            #print('i= {} ;  r[0] = {}'.format(i, r[0]))
+            if r[0] == pi:
+                #mycolors.append('red')
+                mycolors.append([1,0.2,0.2,1])
+            else:
+                #mycolors.append('cyan')
+                mycolors.append([0.8,0.8,0.9,1])
 
-    #ax = cpu_charges_df.plot(kind='bar', color='red')
+        ### XXX
+        #mycolors = list(islice(cycle(['b', 'r', 'g', 'y', 'k']), None, len(cpu_charges_df)))
 
-    ### XXX
-    #mycolors = list(islice(cycle(['b', 'r', 'g', 'y', 'k']), None, len(cpu_charges_df)))
+        #print(mycolors)
 
-    print(mycolors)
+        ax = cpu_charges_df.plot(kind='bar', color=[mycolors], width=0.8)
 
-    ax = cpu_charges_df.plot(kind='bar', color=[mycolors])
+        #ax.set_xticklabels(cpu_charges_df['Last name'])
+        plt.tick_params(
+            axis='x',          # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            bottom=False,      # ticks along the bottom edge are off
+            top=False,         # ticks along the top edge are off
+            labelbottom=False) # labels along the bottom edge are off
+        ax.legend_.remove()
 
-    ax.set_xticklabels(cpu_charges_df['Last name'])
-    ax.legend_.remove()
+        fmt = '$%.0f'
+        #fmt = '$%:,.0f'
+        #fmt = '${x:,.0f}'
+        tick = mtick.FormatStrFormatter(fmt)
+        ax.yaxis.set_major_formatter(tick)
 
-    fmt = '$%.0f'
-    #fmt = '$%:,.0f'
-    #fmt = '${x:,.0f}'
-    tick = mtick.FormatStrFormatter(fmt)
-    ax.yaxis.set_major_formatter(tick)
+        plt.ylabel('CPU charge')
 
-    plt.ylabel('CPU charge')
+        plt.xlabel('PI')
+        #plt.xticks(rotation=45)
+        plt.tight_layout()
 
-    plt.xlabel('PI')
-    #plt.xticks(rotation=45)
-    plt.tight_layout()
+        fig = ax.get_figure()
+        fig.savefig('plot_{}.svg'.format(pi))
 
-    fig = ax.get_figure()
-    fig.savefig('foo_plot.svg')
+        # reset
+        mycolors = []
 
+    return charges_df
+
+
+def make_one_plot():
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Make barchart from csv.')
+    parser.add_argument('-d', '--debug', help='Debugging output', action='store_true')
+    parser.add_argument('reportcsv', type=argparse.FileType('r'), help='Summary usage csv file')
+    args = parser.parse_args()
+
+    print(type(args.reportcsv))
+    print(args.reportcsv.__dict__)
+    make_all_plots(args.reportcsv)
+
+    
 
 if __name__ == '__main__':
-    testing()
-    testing2()
-    main()
+    #testing()
+    #testing2()
+    charges_df = main()
+    #testing3(charges_df)
 
