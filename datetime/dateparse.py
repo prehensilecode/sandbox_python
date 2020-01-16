b561b3f77f8c5d309b9cfe04efce5efa05a2fc79
David Chin <david.w.h.chin@gmail.com>
2020-01-16 16:11:46 -0500

    Another test date

--- datetime/dateparse.py
+++ datetime/dateparse.py
@@ -11,4 +11,15 @@ print(foo.datetime.isoformat())
 bar = delorean.parse(datestr, dayfirst=True)
 print("dayfirst = True")
 print(bar.datetime.isoformat())
+print("")
+
+datestr = "2020-01-09"
+print("datestr = {}".format(datestr))
+foo = delorean.parse(datestr, dayfirst=False)
+print("dayfirst = False")
+print(foo.datetime.isoformat())
+bar = delorean.parse(datestr, dayfirst=True)
+print("dayfirst = True")
+print(bar.datetime.isoformat())
+
 
