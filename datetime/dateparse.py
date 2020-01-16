a1505220abb29a06c36e1a4f0c247bae3b531648
David Chin <david.w.h.chin@gmail.com>
2020-01-16 15:41:43 -0500

    delorean.parse() tests

---
+++ datetime/dateparse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+import os
+import delorean
+
+datestr = "2020-12-03"
+print("datestr = {}".format(datestr))
+foo = delorean.parse(datestr, dayfirst=False)
+print("dayfirst = False")
+print(foo.datetime.isoformat())
+bar = delorean.parse(datestr, dayfirst=True)
+print("dayfirst = True")
+print(bar.datetime.isoformat())
+
