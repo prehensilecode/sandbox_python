5c84b4439f8f5b015a6fb289efce468d3188bd1b
David Chin <david.w.h.chin@gmail.com>
2020-03-05 16:09:13 -0500

    Get months listed

--- pandas/mask_replace.py
+++ pandas/mask_replace.py
@@ -6,7 +6,23 @@ import datetime
 import pandas as pd
 import numpy as np
 
-start = datetime.datetime(2019, 1, 31)
+start = datetime.datetime(2019, 1, 1)
+dates = []
+vals = []
+i = 0.
 for stop in delorean.stops(freq=delorean.MONTHLY, start=start, count=12, timezone='UTC'):
-    print(stop)
+    dates.append(stop.datetime)
+    vals.append(i)
+    i += 1.
 
+print(dates)
+print(vals)
+
+df = pd.DataFrame({'month': dates, 'usage': vals})
+
+print(df)
+
+print(df.dtypes)
+
+mask = df['month'] < datetime.datetime(2019, 7, 1, tzinfo=datetime.timezone.utc)
+print(df[mask])
