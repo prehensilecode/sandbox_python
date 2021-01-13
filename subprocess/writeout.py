7961a3098951c9b479bcb35cbafea6be7445bf7f
David Chin <david.w.h.chin@gmail.com>
2021-01-13 14:02:29 -0500

    Less ambiguous example

--- subprocess/writeout.py
+++ subprocess/writeout.py
@@ -7,5 +7,5 @@ import shutil
 
 ### right?
 with open('foobar.txt', 'w') as f:
-    subprocess.run(['ls', '-l'], stdout=f)
+    subprocess.run(['sinfo', '--Node', '-o', '%12N %.6D %4P %.11T %.4c %.8z %.8m %.8d %.6w %.8f %58E'], stdout=f)
 
