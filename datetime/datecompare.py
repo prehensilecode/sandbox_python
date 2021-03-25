17d6be04deb7b719a9193397e6b560f6a03bbed4
David Chin <david.w.h.chin@gmail.com>
2021-03-25 13:11:55 -0400

    Better examples

--- datetime/datecompare.py
+++ datetime/datecompare.py
@@ -6,20 +6,35 @@ import datetime
 import delorean
 import dateutil
 
-dd = 18344
+# existing expiration (days since epoch)
+dd = 19997
+
+exp = delorean.epoch(0) + datetime.timedelta(days=dd)
+print('existing exp = ', exp)
+print('existing exp.datetime = ', exp.datetime)
+dts = dateutil.parser.parse(str(exp.datetime))
+print('existing dts.date() = ', dts.date())
+print('existing delorean.Delorean(dts) = ', delorean.Delorean(dts))
+
+print('')
 
 proposed_exp_str = '2021/6/14'
 proposed_exp = delorean.parse(proposed_exp_str)
 print('proposed_exp = ', proposed_exp)
 
-exp = delorean.epoch(0) + datetime.timedelta(days=dd)
-print('exp = ', exp)
-print('exp.datetime = ', exp.datetime)
-dts = dateutil.parser.parse(str(exp.datetime))
-print('dts.date() = ', dts.date())
-print('delorean.Delorean(dts) = ', delorean.Delorean(dts))
+if delorean.Delorean(dts) > proposed_exp:
+    print('existing is later than proposed')
+else:
+    print('existing is earlier than proposed')
+
+
+print('')
+
+proposed_exp_str = '2031/2/14'
+proposed_exp = delorean.parse(proposed_exp_str)
+print('proposed_exp = ', proposed_exp)
 
 if delorean.Delorean(dts) > proposed_exp:
-    print('proposed_exp is later than existing')
+    print('existing is later than proposed')
 else:
-    print('proposed_exp is earlier than existing')
+    print('existing is earlier than proposed')
