93dbced17a15308febe839d83bd0e3b7553d284b
David Chin <david.w.h.chin@gmail.com>
2021-05-14 00:00:00 -0400

    Use a list in a dataclass

--- classes/Cheddar.py
+++ classes/Cheddar.py
@@ -8,9 +8,11 @@ class Cheddar:
     unit_cost = 1.50
     weight : float = 0.
     ext_cost : float = weight * unit_cost
+    brands : list[str] = field(default_factory=list)
 
 
 if __name__ == '__main__':
     c = Cheddar(4.)
+    c.brands += ['foo', 'bar', 'baz']
     print(c)
     print(c.unit_cost)
