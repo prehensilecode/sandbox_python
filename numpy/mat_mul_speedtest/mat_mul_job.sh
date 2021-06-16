7d0e4d1488598a6eb44184c5ac3d117786bd3cce
David Chin <david.w.h.chin@gmail.com>
2021-06-16 00:13:37 -0400

    Moved to subdir

--- numpy/mat_mul_speedtest/mat_mul_job.sh
+++ numpy/mat_mul_speedtest/mat_mul_job.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=0:15:00
 #SBATCH --mem=32G
 
-module load python/anaconda3
-#module load python/gcc/3.9.1
+#module load python/anaconda3
+module load python/gcc/3.9.1
 which python3
 
 python3 mat_mul.py
