0cfb76c2e9d6cc503e4037ae3ec7617aac62aed2
David Chin <david.w.h.chin@gmail.com>
2021-06-15 23:37:44 -0400

    Add job script and outputs

---
+++ numpy/mat_mul_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=0:15:00
+#SBATCH --mem=32G
+
+module load python/anaconda3
+#module load python/gcc/3.9.1
+which python3
+
+python3 mat_mul.py
+
