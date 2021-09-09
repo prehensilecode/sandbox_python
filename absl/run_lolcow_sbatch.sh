220bd826f24d94744758f0ff8b84d166b2c7c6e9
David Chin <david.w.h.chin@gmail.com>
2021-09-09 17:57:56 -0400

    Run my version lolcow_mine.sif

--- absl/run_lolcow_sbatch.sh
+++ absl/run_lolcow_sbatch.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-gpu=12
 #SBATCH --mem-per-cpu=40G
 #SBATCH --time=1:00:00
 
-echo $SLURM_NODELIST
+env | grep ^SLURM | sort
 echo $TMPDIR
+nvidia-smi
 
 module load python/gcc
 
 cp external_runscript.sh $TMPDIR
 mkdir $TMPDIR/fakedata
 ls -l $TMPDIR/fakedata
-./run_lolcow.py
+
+if [[ ${SLURM_JOB_PARTITION} = "gpu" ]]
+then
+    args="--use_gpu"
+else
+    args=""
+fi
+
+./run_lolcow.py $args
+
