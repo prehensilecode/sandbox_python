42a9b0a332f8d7211b64da96b8ade180e113ee5e
David Chin <david.w.h.chin@gmail.com>
2021-09-09 13:11:01 -0400

    Add slurm jobscript

---
+++ absl/run_lolcow_sbatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=12
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=1:00:00
+
+echo $SLURM_NODELIST
+echo $TMPDIR
+
+module load python/gcc
+
+cp external_runscript.sh $TMPDIR
+./run_lolcow.py
