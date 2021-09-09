955b9a1be714848b21dfcd7de9498f9a8b96c1f2
David Chin <david.w.h.chin@gmail.com>
2021-09-09 17:56:17 -0400

    In-line check for GPU flag

--- absl/run_lolcow.py
+++ absl/run_lolcow.py
@@ -48,17 +48,17 @@ def main(argv):
 
     print('Using Client.execute(myimg, ...)')
     if FLAGS.use_gpu:
-        cowtext = Client.execute(myimg, ["ls", "-ld", "/fakedata"], environ={'FOO': 'bar'}, bind=[f'{tmpdir}:/mnt', f'{tmpdir}/fakedata:/fakedata'], options=['--nv'])
+        cowtext = Client.execute(myimg, ["ls", "-ld", "/fakedata"], environ={'FOO': 'bar'}, bind=[f'{tmpdir}:/mnt', f'{tmpdir}/fakedata:/fakedata'], options=['--nv'] if FLAGS.use_gpu else None)
         #cowtext = Client.execute(myimg, ["env"], environ={'FOO': 'bar'}, bind=[f'{tmpdir}:/mnt'], options=['--nv'])
         print(cowtext)
         print('')
-        cowtext = Client.execute(myimg, ["ls", "-l", "/mnt"], environ={'FOO': 'bar'}, bind=[f'{tmpdir}:/mnt'], options=['--nv'])
+        cowtext = Client.execute(myimg, ["ls", "-l", "/mnt"], environ={'FOO': 'bar'}, bind=[f'{tmpdir}:/mnt'], options=['--nv'] if FLAGS.use_gpu else None)
         print(cowtext)
         print('')
-        cowtext = Client.execute(myimg, ["nvidia-smi"], environ={'FOO': 'bar'}, bind=f'{tmpdir}:/mnt', options=['--nv'])
+        cowtext = Client.execute(myimg, ["nvidia-smi"], environ={'FOO': 'bar'}, bind=f'{tmpdir}:/mnt', options=['--nv'] if FLAGS.use_gpu else None)
         print(cowtext)
         print('')
-        cowtext = Client.execute(myimg, ["/mnt/external_runscript.sh"], environ={'FOO': 'bar'}, bind=f'{tmpdir}:/mnt', options=['--nv'])
+        cowtext = Client.execute(myimg, ["/mnt/external_runscript.sh"], environ={'FOO': 'bar'}, bind=f'{tmpdir}:/mnt', options=['--nv'] if FLAGS.use_gpu else None)
         print(cowtext)
         print('')
     else:
