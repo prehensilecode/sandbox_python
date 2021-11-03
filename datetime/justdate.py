4e0d7973e06449e0b930d37a89db85ef2f8596d9
David Chin <david.w.h.chin@gmail.com>
2021-11-03 14:53:07 -0400

    Simple datetime.date stuff

---
+++ datetime/justdate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+import os
+import datetime
+
+
+today = datetime.date.today()
+print(f'type(today) = {type(today)}')
+print(f'today = {today}')
+print(f'today.year = {today.year}')
+print(f'today.month = {today.month}')
+print(f'today.day = {today.day}')
