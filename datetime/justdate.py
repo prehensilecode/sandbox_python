74b161e2941f77e174b87906a716ec4dda49ab57
David Chin <david.w.h.chin@gmail.com>
2022-04-26 16:32:30 -0400

    Output format

--- datetime/justdate.py
+++ datetime/justdate.py
@@ -12,6 +12,8 @@ print(f'today.year = {today.year}')
 print(f'today.month = {today.month}')
 print(f'today.day = {today.day}')
 
+print()
+
 foo_date = datetime.date(year=2022, month=4, day=21)
 print(f'type(foo_date) = {type(foo_date)}')
 print(f'foo_date = {foo_date}')
