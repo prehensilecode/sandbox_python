3b39ac2297666bba799e4498269af89fd42d79a2
David Chin <david.w.h.chin@gmail.com>
2022-04-26 16:31:50 -0400

    More date stuff

--- datetime/justdate.py
+++ datetime/justdate.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import datetime
+import delorean
 
 
 today = datetime.date.today()
@@ -10,3 +11,10 @@ print(f'today = {today}')
 print(f'today.year = {today.year}')
 print(f'today.month = {today.month}')
 print(f'today.day = {today.day}')
+
+foo_date = datetime.date(year=2022, month=4, day=21)
+print(f'type(foo_date) = {type(foo_date)}')
+print(f'foo_date = {foo_date}')
+print(f'foo_date.year = {foo_date.year}')
+print(f'foo_date.month = {foo_date.month}')
+print(f'foo_date.day = {foo_date.day}')
