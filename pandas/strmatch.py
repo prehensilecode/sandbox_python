043a74afd1345a69b482bb4c82dbcb8a312c8295
David Chin <david.w.h.chin@gmail.com>
2022-08-30 12:21:37 -0400

    Test pandas str.match

---
+++ pandas/strmatch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+import os
+import pandas as pd
+
+data_list = ['123456', '123456.0', '123456.1', '123456.10']
+
+df = pd.DataFrame(data_list, columns=['JobID'])
+
+print(df)
