1a47e3eec80e245fb0aa7b994cebc5c224c2e320
David Chin <david.w.h.chin@gmail.com>
2022-12-19 12:30:53 -0500

    Expand Tree, Node classes

--- trees/scratch_classes.py
+++ trees/scratch_classes.py
@@ -9,10 +9,21 @@ class Tree():
     def __init__(self, root):
         self.root = root
         self.children = []
+        self.nodes = []
 
     def addNode(self, obj):
         self.children.append(obj)
 
+    def getAllNodes(self):
+        self.nodes.append(self.root)
+        for child in self.children:
+            self.nodes.append(child.data)
+
+        for child in self.children:
+            if child.getChildNodes(self.nodes) != None:
+                child.getChiuldNodes(self.nodes)
+        print(*self.nodes, sep='\n')
+        print(f'Tree size: {len(self.nodes)}')
 
 class Node():
     def __init__(self, data):
@@ -22,6 +33,13 @@ class Node():
     def addNode(self, obj):
         self.children.append(obj)
 
+    def getChildNodes(self, Tree):
+        for child in self.children:
+            if child.children:
+                child.getChildNodes(Tree)
+            Tree.append(child.data)
+
+
 FunCorp = Tree('Head Honcho')
 print(FunCorp.root)
 
@@ -30,7 +48,16 @@ FunCorp.addNode(Node('VP of Shenanigans'))
 FunCorp.addNode(Node('VP of Foobar'))
 
 print(f'C suite: {", ".join(child.data for child in FunCorp.children)}')
+print()
 
 FunCorp.children[0].addNode(Node('GM of Fun'))
+FunCorp.children[1].addNode(Node('GM of Shindigs'))
+FunCorp.children[0].children[0].addNode(Node('Sub-manager of Fun'))
+FunCorp.children[0].children[0].children[0].addNode(Node('Employee of the month'))
 
 print(f'The position under {FunCorp.children[0].data} is {FunCorp.children[0].children[0].data}')
+print()
+
+# get all nodes (unordered)
+FunCorp.getAllNodes()
+
