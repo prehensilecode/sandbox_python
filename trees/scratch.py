4076b1c43e7d14cd18b356cb2a7360ef906dc80c
David Chin <david.w.h.chin@gmail.com>
2022-12-19 12:19:49 -0500

    Add minimal class

--- trees/scratch.py
+++ trees/scratch.py
@@ -96,3 +96,35 @@ for Parent, Children in Families.items():
         print(f"    {Child} has {len(Pets)} pet(s):")
         print(f"        {', and '.join([Pet for Pet in [*Pets]])}")
 print()
+
+
+# Minimal Tree & Node classes
+class Tree():
+    def __init__(self, root):
+        self.root = root
+        self.children = []
+
+    def addNode(self, obj):
+        self.children.append(obj)
+
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def addNode(self, obj):
+        self.children.append(obj)
+
+FunCorp = Tree('Head Honcho')
+print(FunCorp.root)
+
+FunCorp.addNode(Node('VP of Stuff'))
+FunCorp.addNode(Node('VP of Shenanigans'))
+FunCorp.addNode(Node('VP of Foobar'))
+
+print(f'C suite: {", ".join(child.data for child in FunCorp.children)}')
+
+FunCorp.children[0].addNode(Node('GM of Fun'))
+
+print(f'The position under {FunCorp.children[0].data} is {FunCorp.children[0].children[0].data}')
