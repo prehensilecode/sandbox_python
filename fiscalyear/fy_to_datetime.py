5e8367129936a3db5b8dece3f00ae01dfc36ddc1
David Chin <david.w.h.chin@gmail.com>
2023-02-06 12:40:59 -0500

    More FY stuff

--- fiscalyear/fy_to_datetime.py
+++ fiscalyear/fy_to_datetime.py
@@ -10,3 +10,22 @@ print(f'end_date = {end_date}')
 
 for stop in delorean.stops(freq=delorean.MONTHLY, timezone='US/Eastern', start=start_date, stop=end_date):
     print(stop)
+
+fiscalyear.setup_fiscal_calendar(start_month=7)
+drexel_fy = fiscalyear.FiscalYear(2022)
+print(drexel_fy)
+print(drexel_fy.start.year)
+print(drexel_fy.start.month)
+print(drexel_fy.start.day)
+print(type(drexel_fy.start.day))
+
+drexel_start_date = f'{drexel_fy.start.year}-{drexel_fy.start.month:02d}-{drexel_fy.start.day:02d}'
+print(drexel_start_date)
+
+sd = datetime.datetime.fromisoformat(drexel_start_date)
+print(sd)
+
+ed = datetime.datetime.fromisoformat('2023-03-01')
+
+for stop in delorean.stops(freq=delorean.MONTHLY, timezone='US/Eastern', start=sd, stop=ed):
+    print(stop)
