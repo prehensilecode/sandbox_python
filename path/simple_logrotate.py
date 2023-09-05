31d40cd03859415f93f3f5f62b03cc88554fa4ce
David Chin <prehensilecode@users.noreply.github.com>
2023-09-05 16:18:15 -0400

    Fix setting atime & mtime

--- path/simple_logrotate.py
+++ path/simple_logrotate.py
@@ -30,15 +30,17 @@ def compress_maybe(dir, age):
                     print(f'ctime > {age} days -- compressing {f} ...')
 
                 try:
-                    subprocess.check_call(['/bin/xz', f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                except CalledProcessError as error:
+                    subprocess.check_call(['/bin/xz', f],
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE)
+                except subprocess.CalledProcessError as error:
                     print(f'ERROR: {error.cmd} - {error.stderr}')
                     sys.exit(error.returncode)
 
                 # modify atime and mtime if root
                 if os.geteuid() == 0:
                     fxz = Path(str(f) + '.xz')
-                    os.utime(fxz, times=[atime, mtime])
+                    os.utime(fxz, times=(int(atime.timestamp()), int(mtime.timestamp())))
     else:
         if verbose_p:
             print('no uncompressed log files')
@@ -47,39 +49,51 @@ def compress_maybe(dir, age):
 def delete_maybe(dir, expiration):
     global verbose_p
 
-    now = datetime.now()
+    # only delete files in /var/log/...
+    print(f'DEBUG: type(dir) = {type(dir)}')
+    print(f'DEBUG: dir.parts = {dir.parts}')
+    if (dir.parts and
+            dir.parts[0] == '/' and
+            dir.parts[1] == 'var' and
+            dir.parts[2] == 'log'):
+        now = datetime.now()
 
-    file_list = [f for f in dir.glob('*') if f.is_file()]
-    if file_list:
-        for f in file_list:
-            mtime = datetime.fromtimestamp(os.path.getmtime(f))
-            dm = now - mtime
+        file_list = [f for f in dir.glob('*.xz') if f.is_file()]
+        if file_list:
+            for f in file_list:
+                mtime = datetime.fromtimestamp(os.path.getmtime(f))
+                dm = now - mtime
 
-            if dm.days > expiration:
-                if verbose_p:
-                    print(f'ctime > {age} days -- compressing {f} ...')
+                if dm.days > expiration:
+                    if verbose_p:
+                        print(f'ctime > {expiration} days -- deleting {f} ...')
 
-                try:
-                    subprocess.check_call(['/bin/rm', '-f', f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                except CalledProcessError as error:
-                    print(f'ERROR: {error.cmd} - {error.stderr}')
-                    sys.exit(error.returncode)
+                    try:
+                        subprocess.check_call(['/bin/rm', '-f', f],
+                                              stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE)
+                    except subprocess.CalledProcessError as error:
+                        print(f'ERROR: {error.cmd} - {error.stderr}')
+                        sys.exit(error.returncode)
+
 
 def main():
     global verbose_p
 
     parser = argparse.ArgumentParser(description='Simple logfile rotation')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
-    parser.add_argument('-a', '--age', type=int, default=60, help='threshold age for compression in days')
-    parser.add_argument('-e', '--expiration', type=int, default=180, help='threshold age for deletion in days')
+    parser.add_argument('-a', '--age', type=int, default=60,
+                        help='threshold age for compression in days')
+    parser.add_argument('-e', '--expiration', type=int, default=180,
+                        help='threshold age for deletion in days')
     parser.add_argument('directory', metavar='DIR', help='directory')
     args = parser.parse_args()
 
     verbose_p = args.verbose
     dir = Path(args.directory)
     if dir.is_dir():
-        compress_maybe(dir, args.age)
         delete_maybe(dir, args.expiration)
+        compress_maybe(dir, args.age)
     else:
         print(f'ERROR: {dir} is not a directory')
         sys.exit(1)
