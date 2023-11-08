ae73592e1ec4c8fac2a21736835446b42a940a8d
David Chin <prehensilecode@users.noreply.github.com>
2023-11-08 16:59:58 -0500

    Experiments on Python pwd

---
+++ pwd/pwd2dataframe.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import os
+import pwd
+import grp
+import pandas as pd
+
+# from https://ludic.mataroa.blog/blog/i-accidentally-saved-half-a-million-dollars/
+
+everyone = pwd.getpwall()
+
+print(type(everyone[75]))
+print(everyone[75])
+
+data = [{attr: getattr(p, attr) for attr in dir(p) if attr.startswith('pw_')} for p in everyone]
+everyone_df = pd.DataFrame(data)
+print(everyone_df.describe())
+print(everyone_df.head(13))
