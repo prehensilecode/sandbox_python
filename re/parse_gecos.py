4489e74191d419bcb5d6b6394e59af83e8d0d87d
David Chin <prehensilecode@users.noreply.github.com>
2023-12-14 17:22:51 -0500

    Deal with inconsistent gecos

--- re/parse_gecos.py
+++ re/parse_gecos.py
@@ -3,7 +3,7 @@ import sys
 import os
 import re
 
-gecos = "Albert Einstein <Albert.Einstein@advstud.princeton.edu>, RC=1, PI = einstein (some other comment, and maybe more), Created: 1921-04-02 (special guest)"
+gecos = "Albert Einstein <Albert.Einstein@advstud.princeton.edu>, RC=1 PI = einstein (some other comment, and maybe more), Created: 1921-04-02 (special guest)"
 
 name_email_pat = re.compile(r'(.*)\ +<(.*)>')
 
@@ -16,12 +16,22 @@ print(m.group(0))
 print(f'Full name = {m.group(1)}')
 print(f'Email = {m.group(2)}')
 
-rc = int(gecos_split[1].split('=')[1].strip())
-print(f'RC = {rc}')
+#rc_pat = re.compile(r'^RC=(\d)')
+#m = rc_pat.match(gecos_split[1].strip())
+#rc = int(m.group(1))
+#print(f'RC = {rc}')
 
-print(f'gecos_split[2] = {gecos_split[2]}')
-pi_pat = re.compile(r'PI\s=\s(\w+)\s')
-m = pi_pat.match(gecos_split[2].strip())
+#print(f'gecos_split[1] = {gecos_split[1]}')
+#pi_pat = re.compile(r'PI\s=\s(\w+)\s')
+#m = pi_pat.match(gecos_split[2].strip())
+#print(f'm = {m}')
+#pi = m.group(1)
+#print(f'PI = {pi}')
+
+rc_pi_pat = re.compile(r'^RC=(\d)\s+PI\s+=\s+(\w+)\s+.*')
+m = rc_pi_pat.match(gecos_split[1].strip())
 print(f'm = {m}')
-pi = m.group(1)
+rc = int(m.group(1))
+pi = m.group(2)
+print(f'RC = {rc}')
 print(f'PI = {pi}')
