f01d733c6a3ac774a4fb7ecaa5dddb2401a371e9
David Chin <prehensilecode@users.noreply.github.com>
2023-12-15 17:46:45 -0500

    Selection

--- pandas/drop/select_by_value.py
+++ pandas/drop/select_by_value.py
@@ -8,9 +8,23 @@ df = pd.DataFrame({
     'fullname': ['Alice Anderson', 'Bob Bridger', 'Chris Compton', 'Daniel Dumbarton', 'Eric Ericsson', 'Fanny Foster']
     })
 
+print('df =')
 print(df)
 print()
 
 ind = df.loc[df['username'] == 'ddd'].index
+print('ind =')
 print(ind)
+print()
+print('df.loc[ind] =')
 print(df.loc[ind])
+print()
+print('type(df.loc[ind]) = ')
+print(type(df.loc[ind]))
+print()
+print(df.loc[ind][['fullname']])
+print()
+print(type(df.loc[ind][['fullname']].values[0]))
+print()
+
+print(df[df['username'] == 'ddd']['fullname'].values[0])
