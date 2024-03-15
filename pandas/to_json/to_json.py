38fc5a3887739478f7fc137a9b24bafc53bd2d32
David Chin <prehensilecode@users.noreply.github.com>
2024-03-15 16:32:59 -0400

    Deal with datetime as well

--- pandas/to_json/to_json.py
+++ pandas/to_json/to_json.py
@@ -3,9 +3,19 @@ import sys
 import os
 import numpy as np
 import pandas as pd
+import datetime
 
+dt = datetime.timedelta(days=1)
 a_list = ['1-12:00:28', '1 days 12:00:28', '23:14:49']
-a_df = pd.DataFrame(a_list, columns=['Elapsed'])
+b_list = [datetime.datetime.now(), datetime.datetime.now() + dt, datetime.datetime.now() + dt*2]
+
+print(a_list)
+print(b_list)
+
+a_df = pd.DataFrame(
+        {'When': b_list,
+         'Elapsed': a_list
+        })
 
 print(a_df)
 print('')
